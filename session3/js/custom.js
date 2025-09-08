77654f8205bec3a4970670dcdd2d9adabd5c8636
marwaMRadwan <marwaradwan872@hotmail.com>
2025-09-08 10:59:56 +0300

    s3

--- session3/js/custom.js
+++ session3/js/custom.js
@@ -1,7 +1,49 @@
 // document
 // window
+// const d = document.getElementById("my-id")
+// console.log(d);
+// const a = document.getElementsByTagName("p")
+// console.log(a);
+// document.getElementsByClassName("test")
+
+// let d = document.querySelector("p")
+// $(".x")
+// console.log(d.innerText);
+// console.log(d.textContent);
+// console.log(d.innerHTML)
+
+// d.innerHTML="hello<span>abc</span> <script>alert('hi')</script>"
+
+// let btn = document.querySelector("#testbtn")
+// btn.addEventListener("click", function(e){
+//     console.log(btn)
+//     console.log(e.target);
+//     console.log(this);
+//     // this.alert("test")
+//     d.innerText="hello"    
+// })
+
+// let x = document.querySelector(".content")
+// console.log(x.className)
+// x.className = ""
+// console.log(x.getAttribute("data-target"))
+// x.setAttribute("class", "d-block")
+// console.log(x.classList);
+// x.classList.remove("d-none")
+// x.classList.add("test")
+// x.classList.toggle("d-none")
 
 
+// let btns = document.querySelectorAll("#btns button")
+// btns.forEach(
+//     (btn, i) =>{
+//         btn.addEventListener("click", (e)=>{
+//             console.log(`c${i}`)
+//             document.querySelector(`#c${i}`).classList.toggle("d-none")
+//         })
+//     }
+// )
+// document.querySelector('[data-target="a"]').classList.toggle("d-none");
 
 
 // Page Events such as: onload, onunload, onbeforeunload, onpageshow, onpagehide.
