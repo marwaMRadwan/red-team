7fba971bf64356db1df1053f6fcfe7430f58801b
marwaMRadwan <marwaradwan872@hotmail.com>
2025-09-08 09:27:42 +0300

    s3

---
+++ session3/js/custom.js
@@ -0,0 +1,18 @@
+// document
+// window
+
+
+
+
+// Page Events such as: onload, onunload, onbeforeunload, onpageshow, onpagehide.
+// User Interaction Events such as: onclick, ondblclick, onmousedown, onmouseup, onmousemove.
+// Input & Form Events such as: onchange, oninput, onfocus, onblur, onsubmit, onreset.
+// Media Events such as: onplay, onpause, onended, onvolumechange.
+// Animation & Transition Events such as: onanimationstart, onanimationend, ontransitionend.
+// Drag & Drop Events such as: ondrag, ondragover, ondrop.
+// Mouse Events such as: onmouseover, onmouseout, onmouseenter, onmouseleave.
+// Keyboard Events such as: onkeydown, onkeyup, onkeypress.
+// Scroll Events such as: onscroll, onscrollend.
+// Pointer Events such as: onpointerdown, onpointermove, onpointerup.
+// Touch Events such as: ontouchstart, ontouchmove, ontouchend.
+
